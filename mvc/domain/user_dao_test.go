1bfae1feae6fab7309489c995c71e9fb80326631
Renato Martins <renato.martins@neoway.com.br>
2021-04-12 22:29:34 -0300

    Adding benchmarks

---
+++ mvc/domain/user_dao_test.go
@@ -0,0 +1,34 @@
+package domain
+
+import (
+	"net/http"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestGetUserNoUserfound(t *testing.T) {
+	// Initialization:
+
+	// Execution:
+	user, err := GetUser(0)
+
+	// Validation:
+	assert.Nil(t, user, "We were not expeting a user with id 0")
+	assert.NotNil(t, err, "We were expecting a error when user id is 0")
+	assert.EqualValues(t, http.StatusNotFound, err.StatusCode)
+	assert.EqualValues(t, "not_found", err.Code)
+	assert.EqualValues(t, "user 0 was not found", err.Message)
+}
+
+func TestGetUserNoError(t *testing.T) {
+	user, err := GetUser(123)
+
+	assert.Nil(t, err)
+	assert.NotNil(t, user)
+
+	assert.EqualValues(t, 123, user.Id)
+	assert.EqualValues(t, "Fede", user.FirstName)
+	assert.EqualValues(t, "Leon", user.Lastname)
+	assert.EqualValues(t, "myemail@gmail.com", user.Email)
+}
